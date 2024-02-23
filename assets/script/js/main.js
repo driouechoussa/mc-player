a01643fc667b649b45124c289240ac981472ce1c
Ousssama Driouech <thedriwsh@gmail.com>
2024-02-23 17:34:38 +0100

    show the message on the screen

--- assets/script/js/main.js
+++ assets/script/js/main.js
@@ -2,17 +2,19 @@
 //initial the Nav's Buttons from HTML document and add an event click
 var NavButtons = document.querySelectorAll('.navButton');
 
+//call the div square that will be countain the message when the directory not found
+var DisplaySquare = document.getElementById('screen-Displaying');
+
 console.log(LayoutsVisibility());
 
 function LayoutsVisibility() {
     var btnIndex = Array.from(NavButtons);
     NavButtons.forEach(e => { e.addEventListener('click', function() {
             if (btnIndex.indexOf(e) == 0) {
-                console.log('its me');
+                ShowMessagesBox();
             }
             if (btnIndex.indexOf(e) == 1) {
-                setTimeout(() => browseAudioFiles(), 2000);
-                browseAudioFiles();
+                setTimeout(() => browseAudioFiles(), 1000);
             }
             if (btnIndex.indexOf(e) == 2) {
                 
@@ -34,7 +36,35 @@ function LayoutsVisibility() {
 
 function browseAudioFiles() {
     document.getElementById('fileHolder').click();
-    
+
 }
 
+
+function ShowMessagesBox() {
+
+    var MessageBox_drcNotFound = document.createElement('div');
+    MessageBox_drcNotFound.setAttribute('id','MesssageBox-DrcNotFound');
+
+
+    //create image tag and set attribute for it (1)
+    //declaration of tag
+    var MessageBox_Icon = document.createElement('img');
+    //its attribute
+    MessageBox_Icon.setAttribute('src','assets/view/images/patterns/Brocken-cd.png');
+    MessageBox_Icon.setAttribute('class' , 'messagebox-image');
+    MessageBox_Icon.setAttribute('draggable' , 'false');
+
+    //create tag and set attribute for it (2)
+    //declaration of tag
+    var MessageBox_text = document.createElement('h2');
+    //its attribute
+    MessageBox_text.innerText = "there is not any item yet";
+    MessageBox_text.setAttribute('class' , 'messagebox-text');
+
+    //
+    DisplaySquare.appendChild(MessageBox_drcNotFound);
+    MessageBox_drcNotFound.appendChild(MessageBox_Icon);
+    MessageBox_drcNotFound.appendChild(MessageBox_text);
+    
+}
 
